7663f51ff866c78a309030629d56f92a5f502869
eduardopaaaa <eddy.nator@hotmail.com>
2022-08-17 12:41:38 -0600

    heroku fix 2.8

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 app.listen(PORT, () => console.log( 'Listening on port:', PORT));
 
 app.get('/', (req, res) => {
-  res.render('./Views/cover.ejs');
+  res.render('cover.ejs');
 });
 
 // app.get('/ws2/cover', (req, res)=>{
