d2721e0cefb58675d615492fb6b270c23271952d
eduardopaaaa <eddy.nator@hotmail.com>
2022-09-05 17:14:54 -0600

    p3 update

--- P3sub/ip3/backend/server.js
+++ P3sub/ip3/backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const cors = require('cors')
-const VG = require('./models/VG.js')
+const VG = require('./models/vg.js')
 const app = express();
 app.use(express.json())
 app.use(cors())
--- P3sub/ip3/frontend/src/App.js
+++ P3sub/ip3/frontend/src/App.js
@@ -1,22 +1,23 @@
 import axios from 'axios';
+import './App.css';
 import {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const App = () => {
   const[newName, setNewName] = useState('');
-  const[newGenre, setNewGenre] = useState('');
+  const[newCard, setNewCard] = useState('');
   const[newGame, setNewGame] = useState('');
   const[newImg, setNewImg] = useState('');
   const[newVid, setNewVideo] = useState('');
   const[vg, setVg] = useState([]);
-}
+
 
 const handleNewNameChange = (event) => {
   setNewName(event.target.value);
 }
 
-const handleNewGenreChange = (event) => {
-  setNewGenre(event.target.value);
+const handleNewCardChange = (event) => {
+  setNewCard(event.target.value);
 }
 
 const handleNewGameChange = (event) => {
@@ -28,7 +29,7 @@ const handleNewImgChange = (event) => {
 }
 
 const handleNewVideoChange = (event) => {
-  setNewVid(event.target.value);
+  setNewVideo(event.target.value);
 }
 
 
@@ -38,7 +39,7 @@ const handleNewGameSubmit = (event) => {
     'http://localhost:3000/vg',
     {
       library: newName,
-      game: newGenre,
+      game: newCard,
       card: newGame,
       image: newImg,
       video: newVid
@@ -48,7 +49,7 @@ const handleNewGameSubmit = (event) => {
     axios
     .get('http://localhost:3000/vg')
     .then((response) => {
-      setVG(response.data)
+      setVg(response.data)
     })
   })
 }
@@ -57,7 +58,7 @@ useEffect(() => {
   axios
   .get('http://localhost:3000/vg')
   .then((response) => {
-    setVG(response.data)
+    setVg(response.data)
   })
 },[])
 
@@ -68,11 +69,126 @@ const handleDelete = (gameData) => {
     axios
     .get('http://localhost:3000/vg')
     .then((response) => {
-      setVG(response.data)
+      setVg(response.data)
     })
   })
 }
 const handleToggleComplete = (gameData) => {
-  
+  axios
+  .put(`http://localhost:3000/vg/${gameData._id}`,
+  {
+    library: gameData.library,
+    playCheck: !gameData.playCheck,
+    game: gameData.game,
+    card: gameData.card,
+    image: gameData.image,
+    video: gameData.video,
+  }
+  ).then (() => {
+    axios
+    .get('http://localhost:3000/vg')
+    .then((response) => {
+      setVg(response.data)
+    })
+  })
 }
+
+const updateData = (vg) => {
+  axios.put(`http://localhost:3000/vg/${vg._id}`,
+  {
+    library: newName? newName : vg.library,
+    playCheck: vg.playCheck,
+    game: newGame? newGame : vg.game,
+    card: newCard? newCard : vg.card,
+    image: newImg? newImg : vg.image,
+    video: newVid? newVid : vg.video,
+  }
+  ).then(() => {
+    axios.get('http://localhost:3000/vg').then((response) => {
+      setNewName(response.data)
+    })
+  })
+}
+
+return (
+  <main>
+    <h1>Video Game Database</h1>
+    <section>
+      <h2 class='container'>Submit New Game</h2>
+      <form class='form-control' onSubmit={handleNewGameSubmit}>
+  
+        Developer: <input class='form-control' placeholder='Bungie' type='text' onChange={handleNewNameChange}/><br/>
+
+        Game: <input class='form-control' placeholder='Destiny 2' type='text' onChange={handleNewGameChange}/><br/>
+
+        Genre: <input class='form-control' placeholder='FPS-MMO' type='text' onChange={handleNewCardChange}/><br/>
+
+        <input class='form-control' placeholder='Insert Video Game Cover Image' type='url' onChange={handleNewImgChange}/><br/>
+
+        <input class='form-control' placeholder='Insert Video Game GIF' type='url' onChange={handleNewVideoChange}/><br/>
+
+        <input class='btn btn-outline-primary' type='submit' value="SUBMIT"/>
+
+        <br/>
+      </form>
+
+    </section>
+    <section>
+    <br/>
+    <h2 class='container'>Video Game Library</h2>
+    <ul>
+    {
+          vg.map((vg) => {
+            return ( <li
+              
+            key={vg._id}>
+              
+
+              {
+              (vg.playCheck)?
+              <div class='container'><h2 onClick = {() => {
+                handleToggleComplete(vg)
+              }}>{vg.library}</h2></div>
+              :
+              <div class='container' onClick = {() => {
+                handleToggleComplete(vg)
+              }}> <h2>{vg.game} {vg.playCheck? 'Read' : 'Not Read'} </h2> <h3>{vg.card}</h3> 
+              <h2><img src={vg.image}/></h2> </div>
+              }
+              <div>
+              <button class= 'btn btn-outline-danger' onClick={(event) => {handleDelete(vg)}}>DELETE </button>
+			  
+
+              <form class='container' onSubmit={(event) => { updateData(vg)}}>
+
+			  <label> Developer: 
+				<input className='form-control' type='text' defaultValue={vg.library} onChange={handleNewNameChange}/></label><br />
+
+			  <label> Game: <input className='form-control' type='text' defaultValue={vg.game} onChange={handleNewGameChange}/></label><br />
+
+			  <label> Genre: <input className='form-control' type='text' defaultValue={vg.card} onChange={handleNewCardChange}/></label><br />
+
+			  
+
+                <input className="btn btn-primary" type="submit" value="Update" />
+			  </form>
+
+			  <button class= 'btn btn-warning' type="button"  onClick={(event) =>  
+				
+				{handleToggleComplete(vg)}
+			  }
+			  > PLAYED </button><br/>
+              </div>
+              </li> 
+            )
+        })
+      }
+    </ul>
+    </section>
+    
+  </main>
+)
+
+}
+
 export default App;
